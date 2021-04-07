26d5dded339af9a95d8ec665330ac804edbc7636
Rafi Gomez <77979382+Rafael29242@users.noreply.github.com>
2021-04-07 19:11:02 -0400

    Add files via upload

---
+++ LeetCode1313/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LeetCode1313
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(string.Join(", ", RLEListDecompression(new int[] { 4, 5, 4, 10, 2, 5})));
+            Console.WriteLine(string.Join(", ", RLEListDecompression(new int[] { 1, 1, 1, 2, 1, 3, 1, 4, 1, 5})));
+            Console.WriteLine(string.Join(", ", RLEListDecompression(new int[] { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 })));
+            Console.WriteLine(string.Join(", ", RLEListDecompression(new int[] { 5, 1, 4, 2, 3, 3, 2, 4, 1, 5})));
+
+
+        }
+
+        static int[] RLEListDecompression(int[] nums)
+        {
+            var results = new List<int>();
+
+            for (int i = 0; i < nums.Length; i += 2)
+            {
+                for (int j = 0; j < nums[i]; j++)
+                {
+                    results.Add(nums[i + 1]);
+                }
+            }
+
+            return results.ToArray();
+        }
+
+    }
+}
